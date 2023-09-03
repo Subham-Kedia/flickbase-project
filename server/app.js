d2319ffcbdf09368698fbbf0b44cfb4897926dea
shubham kedia <subham.kedia@oriserve.com>
2023-09-04 00:02:04 +0530

    docs(comments  addded for middlewares): sanitization

--- server/app.js
+++ server/app.js
@@ -6,15 +6,30 @@ const bodyParser = require("body-parser")
 const routes = require("./routes")
 
 const { handleError, convertToApiError } = require("./middlewares/errorApi")
-const {jwtStrategy} = require("./middlewares/passport")
+const { jwtStrategy } = require("./middlewares/passport")
 const passport = require("passport")
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}${process.env.DB_HOST}?retryWrites=true&w=majority`
 
 const app = express()
+
+// middleware for parsing application/json
 app.use(bodyParser.json())
+
+// middleware to sanitize xss(cross site scripting) req.body, req.params, req.query
+// must be used before passing to routes
 app.use(xss())
-app.use(mongoSanitize())
+
+// middleware to sanitize mongoDB operator from req.body, req.params, req.query
+// must be used before passing to routes
+app.use(
+  mongoSanitize({
+    onSanitize: ({ req, key }) => {
+      console.warn(`This request[${key}] has been sanitized`, req)
+    },
+  })
+)
+
 mongoose.set("strictQuery", true)
 mongoose.connect(url)
 
@@ -22,7 +37,8 @@ app.use(passport.initialize())
 passport.use("jwt", jwtStrategy)
 
 app.use("/", routes)
-// error handling
+
+// middleware for error handling
 app.use(convertToApiError)
 app.use((err, req, res, next) => {
   handleError(err, res)
